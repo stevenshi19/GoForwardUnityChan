1f6ae0fc8de5467ece64b6350d884db549163794
stevenshi19 <stevenwaseda87@gmail.com>
2020-08-03 15:53:16 +0900

    no message

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour {
 
+  
     // キューブの移動速度
     private float speed = -12;
+
+    Rigidbody2D rigid2D;
 
     //消滅位置
     private float deadLine = -10;
     
     // Use this for initialization
-	void Start () {	
-	}
+	void Start () {
+
+        // Rigidbody2Dのコンポーネントを取得する
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
 	// Update is called once per frame
+
 	void Update () {
         //キューブを移動させる
         transform.Translate(this.speed * Time.deltaTime, 0, 0);
@@ -26,8 +33,13 @@ public class CubeController : MonoBehaviour {
 
     void OnCollisionEnter(Collision other)
     {
+
+         bool isGound = (transform.position.y > -3.0) ? false : true;
+
         if (other.gameObject.tag == "Blocktag") { GetComponent<AudioSource>().Play(); }
 
+        if (isGound == true) { GetComponent<AudioSource>().volume = 1; }
+        
         else { GetComponent<AudioSource>().volume = 0; }
        
     }
